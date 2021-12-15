ff29b74e1c552bd0c4cd4d57e8abab16e2b5cf84
DaanishAli <danish79786@gmail.com>
2021-12-15 08:34:00 +0500

    testimonial final with dummy data

--- save-wastage-food/src/components/testimonial/Testimonial.js
+++ save-wastage-food/src/components/testimonial/Testimonial.js
@@ -3,33 +3,49 @@ import { Carousel } from "react-bootstrap"
 function Testimonial() {
     return (
 
-        <div style={{ backgroundColor: "#0895a4", color:"white"}}>
+        <div style={{ backgroundColor: "#0895a4", color: "white" }}>
             <u className="text-center "><h2>TESTIMONIALS</h2></u>
             <Carousel>
                 <Carousel.Item interval={3000} >
-                    <div className="container pt-5">
-                        <h4 style={{ height: "400px" }}>I congratulate all involved in the noble work at the Fatimid Foundation. The dedication and sheer hard work is very ardent.
-                            I am delighted to have the opportunity of seeing the facilities for myself and I will do my best endeavor to find more support
-                            and financial resources.
-                            Such program needs to be encouraged and maintained Warmest wishes!</h4>
+                    <div className="container " style={{height: "200px"}}>
+                        <div>
+                            <h4 
+                            style={{ margin: 'auto', width: '1000px', paddingTop:"40px"}}
+                            >We have grown to be a leading grassroot humanitarian aid organisation.
+                                Every year we are helping more and more people struck with hardship, all with your help.
+                                We are developing this project to help poor and needy peoples in the Pakistan,
+                                and reduce the food wastage, according to a report, 40 percent of food in Pakistan
+                                is wasted.</h4>
+                        </div>
                     </div>
                 </Carousel.Item>
                 <Carousel.Item interval={3000} >
-                    <div className="container pt-5">
-                        <h4 style={{ height: "400px" }}>I congratulate all involved in the noble work at the Fatimid Foundation. The dedication and sheer hard work is very ardent.
-                            I am delighted to have the opportunity of seeing the facilities for myself and I will do my best endeavor to find more support
-                            and financial resources.
-                            Such program needs to be encouraged and maintained Warmest wishes!</h4>
+                    <div className="container " style={{height: "200px"}}>
+                        <div>
+                            <h4 
+                            style={{ margin: 'auto', width: '1000px', paddingTop:"40px"}}
+                            >We have grown to be a leading grassroot humanitarian aid organisation.
+                                Every year we are helping more and more people struck with hardship, all with your help.
+                                We are developing this project to help poor and needy peoples in the Pakistan,
+                                and reduce the food wastage, according to a report, 40 percent of food in Pakistan
+                                is wasted.</h4>
+                        </div>
                     </div>
                 </Carousel.Item>
                 <Carousel.Item interval={3000} >
-                    <div className="container pt-5">
-                        <h4 style={{ height: "400px" }}>I congratulate all involved in the noble work at the Fatimid Foundation. The dedication and sheer hard work is very ardent.
-                            I am delighted to have the opportunity of seeing the facilities for myself and I will do my best endeavor to find more support
-                            and financial resources.
-                            Such program needs to be encouraged and maintained Warmest wishes!</h4>
+                    <div className="container " style={{height: "200px"}}>
+                        <div>
+                            <h4 
+                            style={{ margin: 'auto', width: '1000px', paddingTop:"40px"}}
+                            >We have grown to be a leading grassroot humanitarian aid organisation.
+                                Every year we are helping more and more people struck with hardship, all with your help.
+                                We are developing this project to help poor and needy peoples in the Pakistan,
+                                and reduce the food wastage, according to a report, 40 percent of food in Pakistan
+                                is wasted.</h4>
+                        </div>
                     </div>
                 </Carousel.Item>
+              
 
             </Carousel>
 
