aeae3089f28ff6fff8c13e199eb4fe0f2f757680
DaanishAli <danish79786@gmail.com>
2021-12-21 11:01:38 +0500

    responsive navbar for mobile tablet and laptops

--- save-wastage-food/src/components/header/Header.js
+++ save-wastage-food/src/components/header/Header.js
@@ -3,47 +3,30 @@ import './header.css'
 
 function Header() {
     return (
-        <header>
-            {/* <div className="container-fluid header text-capitalize " >
-                <div className="container" >
-                    <div className="row ">
-                        <div className='' >
-                            <span>
-                                <strong>Head Office: </strong>
-                                <i className="fas fa-map-marker-alt px-1"></i>
-                                Sarafa Bazar, Boulton Market, Mithadar, Karachi.
-                                <i className="fas fa-phone-alt px-1"></i>
-                                0092 (21) 32413232</span>
-                        </div>
-                        <div className=' header-right '  >
-                            <span className='me-3'> Exemption Certificate</span>
-                            <span>Reports</span>
-                        </div>
+        <header >
+
+            <div className="container-fluid header" >
+                <nav className=" container navbar"
+                    style={{padding: '2px' }}  >
+
+                <span>
+                    < strong > Head Office: </strong>
+                    < i className="fas fa-map-marker-alt " > </i>
+                    Sarafa Bazar, Boulton Market, Mithadar, Karachi.
+                    < i className="fas fa-phone-alt " > </i>
+                    0092(21) 32413232 </span>
+
+                <div className="d-flex  " >
+
+                    <div className=' header-right ' >
+                        < span > Exemption Certificate </span>
+                        < span > Reports </span>
                     </div>
                 </div>
-            </div> */}
-            <div className="container-fluid header">
-                <nav className=" container navbar" style={{padding: '2px'}}>
-
-                        <span >
-                            <strong>Head Office: </strong>
-                            <i className="fas fa-map-marker-alt "></i>
-                            Sarafa Bazar, Boulton Market, Mithadar, Karachi.
-                            <i className="fas fa-phone-alt "></i>
-                            0092 (21) 32413232</span>
-
-                        <div className="d-flex  ">
-
-                            <div className=' header-right '  >
-                                <span > Exemption Certificate</span>
-                                <span>Reports</span>
-                            </div>
-                        </div>
-                </nav>
-            </div>
-
-        </header>
-    )
+            </nav>
+        </div>
+       </header >
+         )
 }
 
 export default Header
