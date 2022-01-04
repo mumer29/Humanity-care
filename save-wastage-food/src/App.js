4a57e3aa04210a8e5781daf26ed08ff8cb8a7fbb
Muhammad Umer <40881824+MUHammadUmerHayat@users.noreply.github.com>
2022-01-04 19:55:48 +0500

    aad file code in files

--- save-wastage-food/src/App.js
+++ save-wastage-food/src/App.js
@@ -30,6 +30,7 @@ const Design2 = () => <h1>Design 2</h1>;
 
 function App() {
 
+  const [inactive, setInactive] = useState(false);
 
 
   return (
@@ -59,7 +60,6 @@ function App() {
       // </Router>
       // <ScrollButton />
     */}
-      const [inactive, setInactive] = useState(false);
 
   return (
     <div className="App">
@@ -72,8 +72,7 @@ function App() {
         />
 
         <div className={`container ${inactive ? "inactive" : ""}`}>
-          {/* improvememt, not recorded in video, its just looping through menuItems
-          instead of hard coding all the routes */}
+         
           {menuItems.map((menu, index) => (
             <>
               <Route key={menu.name} exact={menu.exact} path={menu.to}>
@@ -88,36 +87,6 @@ function App() {
                 : null}
             </>
           ))}
-
-          {/* <Switch>
-            <Route exact path={"/"}>
-              <Dashboard />
-            </Route>
-            <Route exact path={"/content"}>
-              <Content />
-            </Route>
-            <Route path={"/content/courses"}>
-              <Courses />
-            </Route>
-            <Route path={"/content/videos"}>
-              <Videos />
-            </Route>
-            <Route path={"/design"}>
-              <Design />
-            </Route>
-            <Route exact path={"/content-2"}>
-              <Content2 />
-            </Route>
-            <Route path={"/content-2/courses"}>
-              <Courses2 />
-            </Route>
-            <Route path={"/content-2/videos"}>
-              <Videos2 />
-            </Route>
-            <Route path={"/design-2"}>
-              <Design2 />
-            </Route>
-          </Switch> */}
         </div>
       </Router>
     </div>
