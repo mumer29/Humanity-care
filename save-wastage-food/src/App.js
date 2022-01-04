b7dbbbe5d5b8bfecd83c82075eccb38c464d0657
Muhammad Umer <40881824+MUHammadUmerHayat@users.noreply.github.com>
2022-01-04 20:11:35 +0500

    some changes

--- save-wastage-food/src/App.js
+++ save-wastage-food/src/App.js
@@ -61,37 +61,69 @@ function App() {
       // <ScrollButton />
     */}
 
-  return (
-    <div className="App">
-      <Router>
-        <SideMenu
-          onCollapse={(inactive) => {
-            console.log(inactive);
-            setInactive(inactive);
-          }}
-        />
-
-        <div className={`container ${inactive ? "inactive" : ""}`}>
-         
-          {menuItems.map((menu, index) => (
-            <>
-              <Route key={menu.name} exact={menu.exact} path={menu.to}>
-                <h1>{menu.name}</h1>
+    return (
+      <div className="App">
+        <Router>
+          <SideMenu
+            onCollapse={(inactive) => {
+              console.log(inactive);
+              setInactive(inactive);
+            }}
+          />
+  
+          <div className={`container ${inactive ? "inactive" : ""}`}>
+            {/* improvememt, not recorded in video, its just looping through menuItems
+            instead of hard coding all the routes */}
+            {menuItems.map((menu, index) => (
+              <>
+                <Route key={menu.name} exact={menu.exact} path={menu.to}>
+                  <h1>{menu.name}</h1>
+                </Route>
+                {menu.subMenus && menu.subMenus.length > 0
+                  ? menu.subMenus.map((subMenu, i) => (
+                      <Route key={subMenu.name} path={subMenu.to}>
+                        <h1>{subMenu.name}</h1>
+                      </Route>
+                    ))
+                  : null}
+              </>
+            ))}
+  
+            {/* <Switch>
+              <Route exact path={"/"}>
+                <Dashboard />
               </Route>
-              {menu.subMenus && menu.subMenus.length > 0
-                ? menu.subMenus.map((subMenu, i) => (
-                    <Route key={subMenu.name} path={subMenu.to}>
-                      <h1>{subMenu.name}</h1>
-                    </Route>
-                  ))
-                : null}
-            </>
-          ))}
-        </div>
-      </Router>
-    </div>
+              <Route exact path={"/content"}>
+                <Content />
+              </Route>
+              <Route path={"/content/courses"}>
+                <Courses />
+              </Route>
+              <Route path={"/content/videos"}>
+                <Videos />
+              </Route>
+              <Route path={"/design"}>
+                <Design />
+              </Route>
+              <Route exact path={"/content-2"}>
+                <Content2 />
+              </Route>
+              <Route path={"/content-2/courses"}>
+                <Courses2 />
+              </Route>
+              <Route path={"/content-2/videos"}>
+                <Videos2 />
+              </Route>
+              <Route path={"/design-2"}>
+                <Design2 />
+              </Route>
+            </Switch> */}
+          </div>
+        </Router>
       </div>
-  );
-}
-
-export default App;
+      </div>
+    );
+  }
+  
+  export default App;
+  
