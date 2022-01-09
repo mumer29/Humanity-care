dab6edd0e9bd63a84dede15dc125d42f9697c214
DaanishAli <danish79786@gmail.com>
2022-01-09 21:04:26 +0500

    admin dashboard

--- save-wastage-food/src/pages/Customers.jsx
+++ save-wastage-food/src/pages/Customers.jsx
@@ -1,5 +1,5 @@
 // import React from 'react';
-import { firestore, } from "../firebase";
+import { firestore } from "../firebase";
 import React, { useState, useEffect } from 'react';
 
 
@@ -7,69 +7,152 @@ import React, { useState, useEffect } from 'react';
 
 import Table from '../components/table/Table'
 
-import customerList from '../assets/JsonData/customers-list'
+// import customerList from '../assets/JsonData/customers-list.json'
 
 const customerTableHead = [
-    '',
-    'name',
-    'email',
-    'phone',
-    'total orders',
-    'total spend',
-    'location'
+    'amount',
+    'authProvider',
+    'donationType',
+    'donorEmail',
+    'donorName',
+    'donorPhone',
+    'payment '
+
 ]
 
 const renderHead = (item, index) => <th key={index}>{item}</th>
 
 const renderBody = (item, index) => (
     <tr key={index}>
-        <td>{item.id}</td>
-        <td>{item.name}</td>
-        <td>{item.email}</td>
-        <td>{item.phone}</td>
-        <td>{item.total_orders}</td>
-        <td>{item.total_spend}</td>
-        <td>{item.location}</td>
+        <td>{item.amount}</td>
+        <td>{item.authProvider}</td>
+        <td>{item.donationType}</td>
+        <td>{item.phdonorEmailone}</td>
+        <td>{item.donorName}</td>
+        <td>{item.donorPhone}</td>
+        <td>{item.payment}</td>
     </tr>
 )
 
 const Customers = () => {
- 
 
-    const [db, setDb] = useState([
 
-    ]);
-    
+    // const [db, setDb] = useState([]);
+    const [tableData, settableData] = useState("")
+    // console.log(tableData);
+    useEffect(() => {
+        getdata();
+    }, []);
+    console.log("table data", tableData);
 
+
+    async function getdata() {
+
+        let users = [];
+        await firestore.collection("donor").get().then((querySnapshot) => {
+            querySnapshot.forEach(element => {
+                var data = element.data()
+                // console.log("database data", data.amount);
+                //    let result =  Object.values(data);
+                // setDb([...db, data])
+                // settableData({ ...tableData, data })
+
+
+                //    console.log(data); 
+                users.push(data)
+            })
+        })
+        settableData(users)
+        // console.log(users);
+
+        // const ref = firestore().collection("donor").doc();
+        // ref.get().then((doc) => {
+        //   const donordata = doc.data();
+        //   console.log(donordata);
+        //   if (doc.exists) {
+        //     settableData({
+        //       id: donordata.id,
+        //       name: donordata.name,
+        //       email: donordata.email,
+        //       phone: donordata.phone,
+        //       total_orders: donordata.total_orders,
+        //       total_spend: donordata.total_spend,
+        //       location: donordata.location,
+        //       createdAt: donordata.createdAt
+        //     });
+        //   } else {
+        //     console.log("No doc found!");
+        //   }
+        // });
+
+    }
+    // console.log(users);
     // let users = [];
     // users.push(db)
     // useEffect(() => {
     //     postData()
     // }, []);
     // let result = Object.entries(db)
-    let user;
+    // let user;
     // console.log(user);
-    window.addEventListener('load', async (event) => {
-       user = []
+    // window.addEventListener('load', async (event) => {
+    //    user = []
 
-        await firestore.collection("donor").get().then((querySnapshot) => {
-            querySnapshot.forEach(element => {
-                var data = element.data()
-                //    let result =  Object.values(data);
-                // setDb([...db, data])
-                //    console.log(data); 
-                user.push(data)
-            })
-        })
-        // console.log('page is fully loaded');
-        console.log("inner",user);
-    });
-
-    
+    //     await firestore.collection("donor").get().then((querySnapshot) => {
+    //         querySnapshot.forEach(element => {
+    //             var data = element.data()
+    //             //    let result =  Object.values(data);
+    //             // setDb([...db, data])
+    //             //    console.log(data); 
+    //             user.push(data)
+    //         })
+    //     })
+    // console.log('page is fully loaded');
+    // console.log("inner",user);
+    // });
 
 
 
+    // const tableCol = [
+    //     {
+    //         title: " ID",
+    //         field: "id"
+    //     },
+    //     {
+    //         title: "Name",
+    //         field: "name"
+    //     },
+    //     ,
+    //     {
+    //         title: "Email",
+    //         field: "email"
+    //     },
+    //     {
+    //         title: "Phone",
+    //         field: "phone"
+    //     },
+    //     {
+    //         title: " Total Order ",
+    //         field: "total_orders"
+    //     },
+    //     {
+    //         title: "Spend Order",
+    //         field: "total_spend",
 
+    //     },
+    //     {
+    //         title: " Location ",
+    //         field: "location"
+    //     },
+
+    //     {
+    //         title: "Submitted at",
+    //         field: "createdAt"
+    //     }
+    // ];
+
+
+console.log(tableData);
 
     return (
         <div>
@@ -81,13 +164,37 @@ const Customers = () => {
                 <div className="col-12">
                     <div className="card">
                         <div className="card__body">
-                            <Table
-                                // limit='10'
+                            {/* <Table
+                                limit='10'
                                 headData={customerTableHead}
                                 renderHead={(item, index) => renderHead(item, index)}
-                                bodyData={user}
+                                bodyData={customerList}
                                 renderBody={(item, index) => renderBody(item, index)}
-                            />
+                            /> */}
+
+                            <div>
+                                {tableData.map((item, index) => (
+                                    <div>
+                                        <span>{item.donorName}</span>
+                                    </div>
+                                ))}
+                            </div>
+                            {/* <Table
+                                title={"Student's Feedback"}
+                                data={tableData}
+                                columns={tableCol}
+
+                                options={{
+                                    headerStyle: {
+                                        backgroundColor: "#01579b",
+                                        color: "#FFF"
+                                    },
+                                    exportButton: true,
+                                    selection: false,
+                                    search: true
+                                }}
+
+                            /> */}
                         </div>
                     </div>
                 </div>
